49547262490ddf858569663ddd56efd5e54558f0
safarilewis <lleewwii1@gmail.com>
2023-04-06 15:30:09 +0300

    adding primary key

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
 --creates users table with given attributes
-
 CREATE TABLE IF NOT EXISTS users(
-    id int NOT NULL AUTO_INCREMENT,
-    UNIQUE email varchar(255) NOT NULL,
-    name varchar(255),
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    UNIQUE email VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
 );
