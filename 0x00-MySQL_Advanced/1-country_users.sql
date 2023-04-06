80f442b77b6f1fd96ab489bf90f9ca6a568c3b05
safarilewis <lleewwii1@gmail.com>
2023-04-06 17:23:20 +0300

    countrye

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE USERS(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) UNIQUE,
     name VARCHAR(255),
-    country ENUM('US','CO','TN') NOT NULL
+    country ENUM('US','CO','TN') DEFAULT 'US' NOT NULL
 );
