1e00e886af5c3374dc7e6751199c6376be23d21c
safarilewis <lleewwii1@gmail.com>
2023-04-06 15:30:56 +0300

    fixing for if the table exists

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,5 @@
 --creates users table with given attributes
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users(
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     UNIQUE email VARCHAR(255) NOT NULL,
