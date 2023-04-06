8aba5dfdd52056101ee52d3adaf80d01d0b21101
safarilewis <lleewwii1@gmail.com>
2023-04-06 15:57:57 +0300

    task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Creates a table users with given requirements
+DROP TABLE IF EXISTS users;
+CREATE TABLE USERS(
+    id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US','CO','TN') NOT NULL
+);
