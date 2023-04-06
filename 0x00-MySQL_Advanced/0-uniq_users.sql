10f53a488c04fc4d146f1d9b249c0f87724281ff
safarilewis <lleewwii1@gmail.com>
2023-04-06 15:36:00 +0300

    updating...

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 --creates users table with given attributes
 DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
+CREATE TABLE users(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    UNIQUE email VARCHAR(255) NOT NULL,
     name VARCHAR(255)
 );
