d1c3c1b456d3523908829b806b12a25a368dcd74
safarilewis <lleewwii1@gmail.com>
2023-04-11 18:25:06 +0300

    update in mongodb

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""task 10"""
+
+
+def update_topics(mongo_collection, name, topics):
+    '''Updates topics based on the name given'''
+    mongo_collection.update_one(name, topics)
