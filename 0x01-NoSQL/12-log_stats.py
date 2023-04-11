04b917246c950069d5f0a332461335d0899815ed
safarilewis <lleewwii1@gmail.com>
2023-04-11 19:36:28 +0300

    task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''Nginx logs'''
+from pymongo import MongoClient
+
+
+def nginx_logs_collection(nginx_collection):
+    '''Returns information about nginx logs collection'''
+    print("{} logs".format(nginx_collection.count_documents({})))
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        print("\tmethod {}: {}".format(
+            nginx_collection.count_documents({"method": method})))
+    print(nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}))
+
+def start():
+    '''starts connection to mongodb'''
+    client = MongoClient(host="localhost",port=27017)
+    nginx_logs_collection(client.logs.nginx)
+
+if __name__ == "--main__":
+    start()
