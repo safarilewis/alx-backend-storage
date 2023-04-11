581f3a1ca23b61890a8ff3d1f3b5b7ca3ce86fac
safarilewis <lleewwii1@gmail.com>
2023-04-11 17:28:52 +0300

    documentation

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+'''Listing docs in mongodb using pymongo'''
 from pymongo import MongoClient
 import pprint
 
 def list_all(mongo_collection):
+    """Returns collection"""
     if mongo_collection.find() == NULL:
         return []
     for doc in mongo_collection.find():
