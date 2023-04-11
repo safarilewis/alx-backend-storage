21b237c9af17e39c36a60b8de6e91bee770ff4c9
safarilewis <lleewwii1@gmail.com>
2023-04-11 17:33:40 +0300

    task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,9 +3,10 @@
 from pymongo import MongoClient
 import pprint
 
+
 def list_all(mongo_collection):
     """Returns collection"""
     if mongo_collection.find() == NULL:
         return []
     for doc in mongo_collection.find():
-        pprint.pprint(doc)
+        return doc
