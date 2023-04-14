9c890ce861e34c6a9abd1c0a5fb502706642fa8e
safarilewis <lleewwii1@gmail.com>
2023-04-14 17:56:18 +0300

    get int and str

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,7 @@ from typing import Callable, Union, Any
 
 class Cache:
     '''Class that represents a storage container'''
+
     def __init__(self) -> None:
         '''Initializes redis with set params'''
         self._redis = redis.Redis()
@@ -17,3 +18,19 @@ class Cache:
         data_id = str(uuid.uuid4())
         self._redis.set(data_id, data)
         return data_id
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        '''Get method that converts redis strings to UTF-8'''
+        elem = self._redis.get(key)
+        if fn is not None:
+            return fn(elem)
+        else:
+            return elem
+
+        def get_str(self, key: str) -> str:
+            '''Gets a string value from redis'''
+            return self.get(key, lambda x: x.decode('utf-8'))
+
+        def get_int(self, key) -> str:
+            '''Gets an int value from redis'''
+            return self.get(key, lambda x: int(x))
